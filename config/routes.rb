ee9deee8860554917c91b0f780eb040c0860adfd
codeesh <vineshpersaud@gmail.com>
2018-09-18 15:30:06 -0400

    user can now logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'login', to: "sessions#new"
   get 'users/home'
   post "sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
