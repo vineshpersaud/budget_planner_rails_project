1a30c2518ca17541e91388aa79e4bed1e8e95eab
codeesh <vineshpersaud@gmail.com>
2018-09-18 13:33:18 -0400

    add sessions controller update login format

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#signup'
   get 'users/signup'
-  get 'users/login'
+  get 'login', to: "sessions#new"
   get 'users/home'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
