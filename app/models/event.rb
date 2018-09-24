87fe4afb243cb0b08acdb9b1688280cc8267590b
codeesh <vineshpersaud@gmail.com>
2018-09-24 00:47:51 -0400

    add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
  belongs_to :user
  has_many :expenses
 
+ validates :name, presence: true
+ validates :budget , numericality: { greater_than_or_equal_to: 1 }
 
   def budget_difference
     expenses_array = self.expenses.collect { |event| event.total }
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,10 @@
 class Expense < ApplicationRecord
   belongs_to :event
 
+ validates :name, presence: true
+ validates :cost , numericality: { greater_than_or_equal_to: 1 }
+ validates :quantity , numericality: { greater_than_or_equal_to: 1 }
+
   def total 
      return self.cost * self.quantity if self.cost && self.quantity
   end
