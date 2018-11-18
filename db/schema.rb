d3af7b1b7e2b427120a38eeb54047cb7cefacbd1
codeesh <vineshpersaud@gmail.com>
2018-11-17 20:10:57 -0500

    add guest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_212504) do
+ActiveRecord::Schema.define(version: 2018_11_18_010613) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 2018_11_14_212504) do
     t.decimal "cost", precision: 8, scale: 2
     t.integer "quantity"
     t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "guests", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
