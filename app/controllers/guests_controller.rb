8ea788e9b5c7709ae230e07570a33ec726ddccd6
codeesh <vineshpersaud@gmail.com>
2018-11-26 16:35:28 -0500

    add error message to invite

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -10,10 +10,15 @@ class GuestsController < ApplicationController
       @guest = Guest.new(guest_params)
       @guest.save
     end
+    #render:text=>@guest.inspect
     @user_id = session[:user_id]
     @event_id = params[:event_id]
     Invitation.new(event_id: @event_id , guest_id: @guest.id).save
-    redirect_to user_event_path(@user_id,@event_id)
+    if @guest[:name] == ""
+      redirect_to user_event_path(@user_id,@event_id),:flash => { :message => "Please enter new name or pick from list" } 
+    else
+      redirect_to user_event_path(@user_id,@event_id)
+    end
   end
 
   def show
